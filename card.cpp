830b41bcbc8663616fc925bc2016515aa46a5b86
9701836 <70344643+9701836@users.noreply.github.com>
2021-04-30 19:43:15 -0600

    Create card.cpp

---
+++ card.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include "card.h"
+#include <string>
+using namespace std;
+
+Card :: Card(const string c)
+{}
+
+float Card :: display()
+{}
+
+void Card :: print() const{}
