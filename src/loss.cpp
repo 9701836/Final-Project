b958aa151d1446e3bf3a854df6aca0da0b0b0093
9701836 <70344643+9701836@users.noreply.github.com>
2021-04-30 19:46:37 -0600

    Create loss.cpp

---
+++ src/loss.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include "loss.h"
+#include <string>
+using namespace std;
+
+Loss :: Loss(const bool loss)
+{}
+
+string Loss :: lossScreen()
+{}
+void Loss :: print() const{}
