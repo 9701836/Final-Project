ee7fa4c40f7aefe84fec932331589ed6deffa18b
9701836 <70344643+9701836@users.noreply.github.com>
2021-04-30 19:44:10 -0600

    Create card.cpp

---
+++ src/card.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include "card.h"
+#include <string>
+using namespace std;
+
+Card :: Card(const string c)
+{}
+
+float Card :: display()
+{
+  cout << "why";
+}
+
+void Card :: print() const{}
