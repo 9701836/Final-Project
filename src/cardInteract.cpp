f23e9e7e87eb96914e2efe9dc3fd95b514412a3a
9701836 <70344643+9701836@users.noreply.github.com>
2021-04-30 19:45:13 -0600

    Create cardInteract.cpp

---
+++ src/cardInteract.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include "cardInteract.h"
+
+cardInteract :: cardInteract()
+{}
+
+bool cardInteract :: moveCard(const bool cardPresent)
+{}
+bool cardInteract :: noCard(const bool cardPresent)
+{}
+bool cardInteract ::cardFlip(const bool noFace)
+{}
+
+void cardInteract :: print() const{}
