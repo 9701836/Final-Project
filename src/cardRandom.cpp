27bee7492a8bf1972795a87266affe0c7ae1f82e
9701836 <70344643+9701836@users.noreply.github.com>
2021-04-30 19:46:02 -0600

    Create cardRandom.cpp

---
+++ src/cardRandom.cpp
@@ -0,0 +1,11 @@
+#include "cardRandom.h"
+#include <iostream>
+#include <iomanip>
+
+cardRandom :: cardRandom(const int x)
+{}
+
+double cardRandom :: rand()
+{}
+
+void cardRandom :: print() const{}
