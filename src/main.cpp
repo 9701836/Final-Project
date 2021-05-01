e2c444f6eb42b7f01b712743202525044e0eaf43
9701836 <70344643+9701836@users.noreply.github.com>
2021-04-30 19:42:43 -0600

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "card.h"
+#include "cardRandom.h"
+#include "loss.h"
+#include  "victory.h"
+#include "cardInteract.h"
+#include <string>
+using namespace std;
+
+int main() {
+}
