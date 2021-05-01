faf147e9062c8c6e86c8753afa8c9bb68687bb67
9701836 <70344643+9701836@users.noreply.github.com>
2021-04-30 19:47:36 -0600

    Create victory.cpp

---
+++ src/victory.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include "victory.h"
+#include <string>
+using namespace std;
+
+victory :: victory(const bool win)
+{}
+
+string victory :: winScreen()
+{}
+
+void victory :: print() const{}
